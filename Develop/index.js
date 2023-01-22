42ec9546a6c7b4d837c307c683ca594a8a92b21c
Ws538 <williamsu668@gmail.com>
2023-01-22 12:47:07 +1100

    updated code functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Provide instructions on how to install your project:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide information on how to use your project:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Provide lincenses that you want to use on this project",
+    choices: ['MIT', 'GPLv3', 'Apache', 'None']
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Provide contributions:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide examples on how to run tests for your project:",
+  },
+  {
+    type: "input",
+    name: "Contact",
+    message: "Provide contact email for questions:",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (error) => {
+    if (error) throw err;
+    console.log("The file has been saved");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    let markdown = `# ${answers.projectTitle} \n\n`;
+    markdown += `## Description \n\n${answers.description} \n\n`;
+    markdown += `## Installation \n\n${answers.installation} \n\n`;
+    markdown += `## Usage \n\n${answers.usage} \n\n`;
+    markdown +=
+      `## License \n\nThis project is licensed under the ${answers.license} license. \n\n`;
+    markdown += `## Contributing \n\n${answers.contributing} \n\n`;
+    markdown += `## Tests \n\n${answers.tests} \n\n`;
+    markdown += `## Questions \n\n Please contact me in the following email: \n ${answers.Contact} \n\n`
+    writeToFile("README.md", markdown);
+  });
+}
 
-// Function call to initialize app
 init();
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# README-generator
+# Readme Generator  
+
+## Description 
+
+Every project requires you to make a readMe doc, but it gets so annoying after a while. Therefor i have decided to create a readme Generator using Node.js,  
+
+## Installation 
+
+Simply clone this repo, Download npm inquirer 8.2.4 and run node index.js 
+
+## Usage 
+
+Once you have ran node index.js, simply answer the following prompt questions and it will generate your easily made readMe.md file 
+
+## License 
+
+This project is licensed under the None license. 
+
+## Contributing 
+
+N/A 
+
+## Tests 
+
+Click this link to watch a short video on how to use this project : https://drive.google.com/file/d/18DyI9VJLWnwiK0Zp4Q8IUpUfIwiDmzP5/view 
+
+## Questions 
+
+ Please contact me in the following email: 
+ Williamsu668@gmail.com 
+
